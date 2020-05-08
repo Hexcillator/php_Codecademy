43406fcdd5f5b3808f6970b894a9010f3a905098
Patricio <concertmate@btinternet.com>
2020-05-08 17:50:01 +0100

    first commit

---
+++ tesst1.php
@@ -0,0 +1,17 @@
+<<?php
+  $alien_name = "cholgo";
+  $favorite_name =& $alien_name;
+
+  $favorite_name ="chaguan";
+  $alien_name = "bengo";
+
+  echo $favorite_name;
+
+  echo 1 + 3 * (9 / 3) ** (1 + 1);
+
+
+
+
+
+
+ ?>
