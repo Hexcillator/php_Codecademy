529c39eaca0c1f027b05b03e0f1aaba0eaf84119
Patricio <concertmate@btinternet.com>
2020-05-14 00:53:17 +0100

    fixed the result to display only two decimal numbers

--- world_traveller.php
+++ world_traveller.php
@@ -24,9 +24,22 @@ echo "\nDanish Krone= ".$krones / $krones_rate." Pounds";
 echo "\nlek= ".$lek / $lek_rate." Pounds";
 
 echo "\nTotal conversion including 1 Pound comission per transaction:\n";
+//echo ($riel / $riel_rate)+1+($kyat / $kyat_rate)+1+($krones / $krones_rate)+1+($lek / $lek_rate);
+//if we want to get rid of the many decimals and only get only two decimal places
+
+$final_amount=($riel / $riel_rate)+1+($kyat / $kyat_rate)+1+($krones / $krones_rate)+1+($lek / $lek_rate);
+$pounds_only=$final_amount % 10000000;
+$pounds_pences = $final_amount - $pounds_only;
+$pounds_pences *= 100;
+$pounds_pences %= 1000;
+$pounds_pences /= 100;
+$total_pounds = $pounds_only + $pounds_pences;
+
+echo "£".$total_pounds;
 
 
-echo ($riel / $riel_rate)+1+($kyat / $kyat_rate)+1+($krones / $krones_rate)+1+($lek / $lek_rate);
+
+
 
 
 
